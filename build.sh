85b031f8d627e6589e517bc8a7b43e1412d9e400
javad <ja7adr@gmail.com>
2021-05-27 08:13:05 +0430

    fix build path

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 dt=$(date '+%d/%m/%Y');
 
 bundle exec jekyll build
-cp -r _site/* /home/javad/Project/other/Blog/blog
-cd /home/javad/Project/other/Blog/blog
+cp -r _site/* /home/javad/Work/Project/other/blog
+cd /home/javad/Work/Project/other/blog
 git pull
 git add .
 git commit -m "Update $dt"
